704695b34018218e0827211422aeacb05a3f7760
rajatgoyal715 <rajatgoyal715@gmail.com>
2016-08-19 10:26:06 +0530

    Add files via upload

---
+++ Implementation/DivisibleSumPairs.java
@@ -0,0 +1,23 @@
+package Implementation;
+
+import java.util.*;
+
+public class DivisibleSumPairs {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        int c=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if((a[i]+a[j])%k==0)
+                    c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
