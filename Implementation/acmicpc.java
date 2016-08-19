3329a32aa974baabfbe16a417b46df4393330164
rajatgoyal715 <rajatgoyal715@gmail.com>
2016-08-19 10:23:05 +0530

    Update acmicpc.java

--- Implementation/acmicpc.java
+++ Implementation/acmicpc.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hackerrank;
 
 import java.util.Scanner;
-/**
- *
- * @author mukul goyal
- */
+
 public class acmicpc {
         public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
