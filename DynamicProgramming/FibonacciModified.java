ff21b77858b52c843bc840bc6f7dd5f42d10697a
rajatgoyal715 <rajatgoyal715@gmail.com>
2016-08-26 00:53:49 +0530

    Add files via upload

---
+++ DynamicProgramming/FibonacciModified.java
@@ -0,0 +1,24 @@
+package DynamicProgramming;
+
+import java.util.*;
+import java.math.BigInteger;
+
+/*
+ * @author -- rajatgoyal715
+ */
+
+public class FibonacciModified {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        int n = sc.nextInt();
+        BigInteger temp = BigInteger.valueOf(1);
+        for(int i=3;i<=n;i++){
+            temp = (b.multiply(b)).add(a);
+            a = b;
+            b = temp;
+        }
+        System.out.println(temp);
+    }
+}
---
+++ DynamicProgramming/TheMaximumSubarray.java
@@ -0,0 +1,34 @@
+package DynamicProgramming;
+
+import java.util.*;
+import java.io.*;
+
+/*
+ * @author -- rajatgoyal715
+ */
+
+public class TheMaximumSubarray {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t--!=0){
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for(int i=0;i<n;i++)
+                a[i] = sc.nextInt();
+            int max=a[0],cmax=0;
+            int sum=0,max2=Integer.MIN_VALUE;
+            for(int i=0;i<n;i++){
+                if(a[i]>max2)
+                    max2=a[i];
+                if(a[i]>0)
+                    sum+=a[i];
+                cmax = Math.max(a[i],cmax+a[i]);
+                max = Math.max(max,cmax);
+            }
+            if(max2<0)
+                sum=max2;
+            System.out.println(max+" "+sum);
+        }
+    }
+}
