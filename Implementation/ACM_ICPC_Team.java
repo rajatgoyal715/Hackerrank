809d46adcf5937c61927c7b8e1e599a533ecb127
rajatgoyal715 <rajatgoyal715@gmail.com>
2016-08-27 17:38:48 +0530

    Update ACM_ICPC_Team.java

--- Implementation/ACM_ICPC_Team.java
+++ Implementation/ACM_ICPC_Team.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Implementation;
 
 import java.util.Scanner;
 /**
- *
- * @author mukul goyal
+ * @author rajatgoyal715
  */
 public class ACM_ICPC_Team {
     public static void main(String[] args){
