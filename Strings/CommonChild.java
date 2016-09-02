02e7f039c3c1c386b7c4d647b7826527e5b22d41
rajatgoyal715 <rajatgoyal715@gmail.com>
2016-09-02 21:42:45 +0530

    Add files via upload

---
+++ Strings/CommonChild.java
@@ -0,0 +1,36 @@
+package Strings;
+
+import java.util.*;
+import java.io.*;
+
+/*
+ * @author -- rajatgoyal715
+ */
+
+public class CommonChild {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        String s2=sc.nextLine();
+        char a[]=s1.toCharArray();
+        char b[]=s2.toCharArray();
+        int m=a.length;
+        int n=b.length;
+        int arr[][]=new int[m+1][n+1];
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0||j==0)
+                    arr[i][j]=0;
+                else{
+                    if(a[i-1]==b[j-1]){
+                        arr[i][j]=arr[i-1][j-1]+1;
+                    }
+                    else{
+                        arr[i][j]=Math.max(arr[i-1][j],arr[i][j-1]);
+                    }
+                }
+            }
+        }
+        System.out.println(arr[m][n]);
+    }
+}
---
+++ Strings/StringFunctionCalculation.java
@@ -0,0 +1,37 @@
+package Strings;
+
+import java.util.*;
+import java.io.*;
+
+/*
+ * @author -- rajatgoyal715
+ */
+
+public class StringFunctionCalculation {
+    static int l;
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String t=sc.nextLine();
+        l=t.length();
+        int max=0;
+        for(int i=0;i<l;i++){
+            for(int j=i+1;j<=l;j++){
+                int temp=f(t.substring(i,j),t);
+                if(temp>max)
+                    max=temp;
+            }
+        }
+        System.out.println(max);
+    }
+    
+    public static int f(String s,String t){
+        int l1=s.length();
+        int count=0;
+        for(int i=0;i<=l-l1;i++){
+            if(t.substring(i,i+l1).equals(s))
+                count++;
+        }
+        return count*l1;
+    }
+}
