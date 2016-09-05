4b61625d9bb31ad6a367a572375c59c2b9541f7c
rajatgoyal715 <rajatgoyal715@gmail.com>
2016-09-05 23:55:45 +0530

    Add files via upload

--- Implementation/AngryProfessor.java
+++ Implementation/AngryProfessor.java
@@ -1,12 +1,9 @@
-/*
- *  Add any license header using tools << templates << licenses << default license
- */
-
 package Implementation;
 
-/**
- * @author mukul goyal
+/*
+ * @author -- rajatgoyal715
  */
+
 import java.util.Scanner;
 
 public class AngryProfessor {
