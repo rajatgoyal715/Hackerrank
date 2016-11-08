f80b567343afb76334f418b92b5b06403881d8d5
Rajat Goyal <rajatgoyal715@gmail.com>
2016-11-08 22:26:41 +0530

    Added more files

---
+++ Implementation/EqualizeTheArray.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class EqualizeTheArray {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        int b[]=new int[101];
+        int max=0;
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+            b[a[i]]++;
+            if(max<b[a[i]]){
+                max=b[a[i]];
+            }
+        }
+        System.out.println(n-max);
+    }
+}
---
+++ Implementation/TheTimeInWords.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class TheTimeInWords {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int num1,num2;
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        String time1[] = {"one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve"};
+        String time2[] = {" o' clock","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve",
+            "thirteen","fourteen","fivteen","sixteen","seventeen","eighteen","nineteen","twenty","twenty one","twenty two","twenty three","twenty four",
+            "twenty five","twenty six","twenty seven","twenty eight","twenty nine","half past "};
+        String output="";
+        if(num1!=0)
+            output = time1[num1-1];
+        if(num2==0)
+            output = output + time2[num2];
+        else if(num2<30){
+            if(num2==15)
+                output = "quarter past " + output;
+            else
+                output = time2[num2] + ((num2==1)?" minute ":" minutes ") + "past " + output;
+        }
+        else if(num2==30)
+            output = time2[num2] + output;
+        else if(num2>30){
+            if(num2==45)
+                output = "quarter to " + time1[num1];
+            else
+                output = time2[60-num2] + ((num2==59)?" minute ":" minutes ") + "to " + time1[num1];
+        }
+        System.out.println(output);
+    }
+}
