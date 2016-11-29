2dc0a5ad24277050b22636d31635cfd053ca1f07
Rajat Goyal <rajatgoyal715@gmail.com>
2016-11-29 19:58:33 +0530

    Added one file

---
+++ Implementation/BetweenTwoSets.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+/**
+ * Created by ssdn on 11/29/2016.
+ */
+
+public class BetweenTwoSets {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        int[] b = new int[m];
+        for(int b_i=0; b_i < m; b_i++){
+            b[b_i] = in.nextInt();
+        }
+        int count=0;
+        for(int i=1;i<=100;i++){
+            boolean a1=true,a2=true;
+            label1:
+            for(int j=0;j<n;j++){
+                if(i%a[j]!=0){
+                    a1=false;
+                    break label1;
+                }
+            }
+            label2:
+            for(int j=0;j<m;j++){
+                if(b[j]%i!=0){
+                    a2=false;
+                    break label2;
+                }
+            }
+            if(a1&&a2)
+                count++;
+        }
+        System.out.println(count);
+    }
+}
