e43e27d8cf4af9a0824c68519483c88571f4e8aa
rajatgoyal715 <rajatgoyal715@gmail.com>
2017-06-07 09:51:35 +0530

    Added files

--- Recursion/ThePowerSum.java
+++ Recursion/ThePowerSum.java
@@ -1,7 +1,5 @@
-package Recursion;
-
 import java.util.*;
-import java.io.*;
+import java.util.*;
 
 /**
  * Created by rajat goyal on 3/19/2017.
@@ -9,6 +7,23 @@ import java.io.*;
 
 public class ThePowerSum {
     public static void main(String[] args) {
-        // 
+    	Scanner sc = new Scanner(System.in);
+    	int x = sc.nextInt();
+    	int n = sc.nextInt();
+    	int a = (int)Math.pow(x,(1/n));
+      System.out.println(power(x, n, a));
+    }
+
+    public static int power(int x, int n, int a) {
+    	if(x<0) return 0;
+    	if(x==0) return 1;
+    	if(x==1) return 1;
+    	int sum = 0;
+    	for(int i=a;i>=1;i--){
+    		int temp = x-i*i;
+    		int b = (int)Math.pow(temp, (1/n));
+    		sum += power(temp, n, b);
+    	}
+    	return sum;
     }
 }
