3e17de55b4d174ab60e152d4c55833aaf801b413
Rajat Goyal <rajatgoyal715@gmail.com>
2018-03-17 19:37:31 +0530

    Add solutions of :
    * 5 Implementation problems
    * 1 Sorting problem
    * 1 Strings problem
    * 2 Warmup problems

---
+++ Implementation/CircularArrayRotation.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ * @author -- rajatgoyal715
+ */
+
+public class CircularArrayRotation {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s[] = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int k = Integer.parseInt(s[1]);
+        int q = Integer.parseInt(s[2]);
+        k = k%n;
+        int a[] = new int[n];
+        String s1[] = br.readLine().split(" ");
+        for(int i=0;i<n;i++){
+            a[(i+k)%n] = Integer.parseInt(s1[i]);
+        }
+        while(q-->0){
+            System.out.println(a[Integer.parseInt(br.readLine())]);
+        }
+    }
+}
