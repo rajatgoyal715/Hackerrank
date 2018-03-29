4710c852051677fae2247846f3057f7bcf0d42e2
Rajat Goyal <rajatgoyal715@gmail.com>
2018-03-29 08:13:49 +0530

    Add one more solution

---
+++ ConstructiveAlgorithms/NewYearChaos.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+class NewYearChaos {
+	static void minimumBribes(int[] q) {
+        int ans = 0;
+        for (int i = q.length - 1; i >= 0; i--) {
+            if (q[i] - (i + 1) > 2) {
+                System.out.println("Too chaotic");
+                return;
+            }
+            for (int j = Math.max(0, q[i] - 2); j < i; j++)
+                if (q[j] > q[i]) ans++;
+        }
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int[] q = new int[n];
+            for(int q_i = 0; q_i < n; q_i++){
+                q[q_i] = in.nextInt();
+            }
+            minimumBribes(q);
+        }
+        in.close();
+    }
+}
