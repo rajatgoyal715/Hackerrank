2009e496998831866f89ffc5ee23082d1e569d34
Rajat Goyal <rajatgoyal715@gmail.com>
2018-08-02 01:01:46 +0530

    Add 7 more solutions of Java domain

---
+++ Java/Introduction/JavaDatatypes.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class JavaDatatypes {
+    public static void main(String[] argh) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            try {
+                long x = sc.nextLong();
+                System.out.println(x + " can be fitted in:");
+                if (x >= -128 && x <= 127)
+                    System.out.println("* byte");
+                if (x >= -32768 && x <= 32767)
+                    System.out.println("* short");
+                if (x >= -Math.pow(2, 31) && x <= Math.pow(2, 31) - 1)
+                    System.out.println("* int");
+                if (x >= -Math.pow(2, 63) && x <= Math.pow(2, 63) - 1)
+                    System.out.println("* long");
+            } catch (Exception e) {
+                System.out.println(sc.next() + " can't be fitted anywhere.");
+            }
+        }
+    }
+}
