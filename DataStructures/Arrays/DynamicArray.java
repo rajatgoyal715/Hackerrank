fade8190f65bd4f465e40264ceaad02819eeb9d0
Rajat Goyal <rajatgoyal715@gmail.com>
2018-08-02 18:08:06 +0530

    Add solutions of Arrays sub-domain of DataStructures domain

---
+++ DataStructures/Arrays/DynamicArray.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class DynamicArray {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1[] = br.readLine().split(" ");
+        int n = Integer.parseInt(s1[0]);
+        int q = Integer.parseInt(s1[1]);
+
+        // arraylist of arraylist
+        ArrayList<ArrayList<Integer>> al = new ArrayList<ArrayList<Integer>>(n);
+
+        // initialise that there are a total of n lists..
+        for (int i = 0; i < n; i++) {
+            ArrayList<Integer> sub = new ArrayList<Integer>();
+            al.add(sub);
+        }
+        int lastans = 0;
+        while (q-- > 0) {
+            String s[] = br.readLine().split(" ");
+            int num = Integer.parseInt(s[0]);
+            int x = Integer.parseInt(s[1]);
+            int y = Integer.parseInt(s[2]);
+            if (num == 1) {
+                int temp = (x ^ lastans) % n;
+                // getting sub arraylist with index equals to temp and then adding the value of
+                // y in it.
+                al.get(temp).add(y);
+            } else {
+                int temp = (x ^ lastans) % n;
+                // finsing size of sub arraylist
+                int a1 = al.get(temp).size();
+                // finding integer stored at index in the subarraylist of index equals to temp
+                lastans = al.get(temp).get(y % a1);
+                // print statement
+                System.out.println(lastans);
+            }
+        }
+    }
+}
---
+++ DataStructures/Arrays/LeftRotation.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class LeftRotation {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int d = sc.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+        d = d % n;
+        for (int i = 0; i < d; i++) {
+            rotate(a);
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+    public static void rotate(int[] a) {
+        int temp = a[0];
+        int n = a.length;
+        for (int i = 0; i < n - 1; i++) {
+            a[i] = a[i + 1];
+        }
+        a[n - 1] = temp;
+    }
+}
