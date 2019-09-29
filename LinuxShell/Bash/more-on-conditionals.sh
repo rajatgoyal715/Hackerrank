eea9852a2f14c3681bfb1a6409701ca7f7f60f29
Rajat Goyal <rajatgoyal715@gmail.com>
2019-09-29 22:51:40 +0530

    Add solution to conditional problem

---
+++ LinuxShell/Bash/more-on-conditionals.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read x
+read y
+read z
+
+if [ $x -eq $y ] && [ $x -eq $z ]; then
+	echo "EQUILATERAL"
+elif [ $x != $y ] && [ $x != $z ] && [ $y != $z ]; then
+	echo "SCALENE"
+else
+	echo "ISOSCELES"
+fi
