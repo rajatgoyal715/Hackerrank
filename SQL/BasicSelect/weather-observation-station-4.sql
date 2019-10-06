ef9976f1dfc65a307482de76e2176a78b0644597
Rajat Goyal <rajatgoyal715@gmail.com>
2019-10-07 00:24:15 +0530

    Add solutions of SQL domain

---
+++ SQL/BasicSelect/weather-observation-station-4.sql
@@ -0,0 +1,5 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT COUNT (CITY) - COUNT (DISTINCT CITY) FROM STATION
